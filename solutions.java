8770d56d445630b86f789c0a7c98167b4bd8b38a
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2023-09-03 22:13:43 +0530

    Add files via upload

---
+++ solutions.java
@@ -0,0 +1,24 @@
+package solutions;
+
+
+public class Solutions {
+
+    
+    public static void main(String[] args) {
+       
+
+         int n = 5;
+
+   for(int i=1; i<=n; i++){
+    //spaces 
+    for(int j=1; j<=n-i; j++){
+        System.out.print(" ");
+    }
+    // numbers -> print row no, row no times 
+    for(int j=1; j<=i; j++){
+    System.out.print(i + " ");
+   }   
+    System.out.println("");
+}
+}
+}
