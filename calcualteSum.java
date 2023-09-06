5fd9f3df642aa051c4177ad4510f90c12562a0c4
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2023-09-06 12:17:35 +0530

    Add files via upload

---
+++ calcualteSum.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+public class Solution {
+public static int calculateSum(int a, int b){
+    
+   int sum = a + b;
+    return sum;
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       int a =sc.nextInt();
+       int b =sc.nextInt();
+       int sum = calculateSum(a,b);
+        System.out.println("Sum of 2 no is:"+sum);
+        
+        
+    }
+    
+}
