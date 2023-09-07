1a17de7d4583a5e9d1d006eb358fab869e75ae3e
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2023-09-07 22:10:05 +0530

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,18 @@
+package solution;
+
+import java.util.*;
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       int a =sc.nextInt();
+       int b =sc.nextInt();
+       int c =sc.nextInt();
+       int d =sc.nextInt();
+       int e =sc.nextInt();
+       int average = (a + b + c + d + e )/5;
+        System.out.println("The average value of:"+average);
+        
+    }
+    
+}
