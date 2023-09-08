b4ade341d07abbf3f86d2d7edf7cf3b982f86635
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2023-09-08 21:17:21 +0530

    This code for array

---
+++ Array.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class Arrays {
+
+   
+    public static void main(String[] args) {
+       int []marks = new int[3];
+       marks [0] = 85;//phy
+       marks [1] = 89;//math
+       marks [2] = 98;//eng
+        System.out.println(marks [0]);
+        System.out.println(marks [1]);
+        System.out.println(marks [2]);
+
+    }
+    
+}
