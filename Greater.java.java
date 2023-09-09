5fac5c1ac7a71706341428f885c0b19c3abb1373
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2023-09-09 20:24:36 +0530

    Add files via upload

---
+++ Greater.java.java
@@ -0,0 +1,25 @@
+ackage solutions;
+
+import java.util.*;
+public class Solutions {
+    public static int getGreater(int a, int b){
+        if(a>b){
+            return a;
+        }
+        else{
+            return b;
+        }
+    }
+
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(getGreater(a,b));
+       
+
+         }
+     
+
+      }
