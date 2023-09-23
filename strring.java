eda21edc7693ef6a3b321a7fa5ce6ddba39140b7
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2023-09-24 01:46:52 +0530

    Add files via upload
    This code is count how many char given your input  and display the total char

---
+++ strring.java
@@ -0,0 +1,13 @@
+public class string{ {
+    public static void main(String[] args) {
+      //concatation
+      String firstName = "kaif";
+      String lastName =  "ali";
+      String fullName = firstName+ "@" + lastName;
+      //kaif@ali
+      System.out.println(fullName.length());
+
+    }
+
+    
+}
