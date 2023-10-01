293ad40dfc701867bbd1c88f3c088ecdd0199430
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2023-10-01 10:16:44 +0530

    Add files via upload
    This is mini calculator

---
+++ calculator.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class welcomemyworld {
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int operator = sc.nextInt();
+    /** 
+     * 1 -> +
+     * 2 -> -
+     * 3 -> *
+     * 4 -> /
+     * 5 -> %
+     */
+    switch(operator){
+        case 1: System.out.println(a+b);
+        break;
+        case 2: System.out.println(a-b);
+        break;
+        case 3: System.out.println(a*b);
+        break;
+        case 4: if(a==0){
+            System.out.println("Invalid Division");
+        }
+        else{
+            System.out.println(a/b);
+        }
+        break;
+        case 5: if(b==0){
+            System.out.println( "Invalid Division");
+        }
+        else{
+            System.out.println(a%b);
+        }
+        break;
+        default: System.out.println("Invalid operator");
+    }
+    
+    }
+}
