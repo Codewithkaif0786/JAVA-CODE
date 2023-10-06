205b1236bad66ac3a36b74e12fed16d4b255a525
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2023-10-06 18:33:56 +0530

    Add files via upload
    This code is take two no of sqare

---
+++ Square.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ public class exercies {
+public static void main(String[] args) {
+    System.out.println("Enter x");
+    Scanner sc = new Scanner(System.in);
+    int x = sc.nextInt();
+    System.out.println("Enter n");
+    int n = sc.nextInt();
+
+    int result = 1;
+    // please see that n is not too large or else result will exceed the size of int
+    for(int i=0; i<n; i++){
+        result = result * x;
+    }
+    System.out.println("x to the power n is : "+result);
+    }
+}
