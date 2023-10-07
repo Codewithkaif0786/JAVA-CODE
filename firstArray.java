908a5d8f7abd1a6a83608aae3ddc2eb2316b1249
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2023-10-07 09:39:49 +0530

    Add files via upload

---
+++ firstArray.java
@@ -0,0 +1,14 @@
+public class Arayexercise {
+public static void main(String[] args) {
+        int[] marks = new int[3];
+        marks[0] = 97; //phy
+        marks[1] = 98; //che
+        marks[2] = 95; //math
+          for(int i=0; i<=3; i++){
+            System.out.println(marks[i]);
+        }
+    }
+   
+    
+    
+}
