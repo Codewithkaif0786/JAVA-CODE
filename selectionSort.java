6bf9ec4392d4b1c97900cc2cbbad6f16488bff6c
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2023-10-14 12:04:16 +0530

    Add files via upload

---
+++ selectionSort.java
@@ -0,0 +1,30 @@
+public class Bits {
+     // Selection sort
+    public static void printArray(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {7,8,3,1,2};
+
+        //Selection sort
+        for(int i=0; i<arr.length-1; i++){
+            int smallest = i;
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[smallest]>arr[j]){
+                    smallest = j;
+                }
+
+            }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+
+        }
+        printArray(arr);
+    }
+
+}
+
