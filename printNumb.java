411510e95487cf65c321282a023556e39ed959ec
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2023-10-17 16:21:51 +0530

    Add files via upload

---
+++ printNumb.java
@@ -0,0 +1,15 @@
+public class Recursion1 {
+    
+     public static void printNumb(int n){
+        if(n == 6){
+            return;
+        }
+        System.out.println(n);
+        printNumb(n+1);
+    }
+    public static void main(String[] args) {
+        int n = 1;
+        printNumb(n);
+
+    }
+}
