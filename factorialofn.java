53757ac7764f15a3ba6a297df062b8d6fa4eac8d
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2023-10-19 20:37:39 +0530

    Add files via upload

---
+++ factorialofn.java
@@ -0,0 +1,20 @@
+public class Recursion1 {
+
+     // Qs. Print Factorial of a number n
+    public static int calcFactorial(int n){
+        if(n ==1 || n == 0){
+            return 1;
+        }
+        int fact_nm1 = calcFactorial(n-1);
+        int fact_n = n * fact_nm1;
+        return fact_n;
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        int ans = calcFactorial(n);
+        System.out.println(ans);
+
+        
+    }
+
+}
