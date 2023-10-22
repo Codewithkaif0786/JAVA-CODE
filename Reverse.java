e322833e8f743d7e6802f743991dc1e3d5357ace
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2023-10-22 20:24:57 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,17 @@
+public class Recursion2 {
+
+     public static void printRev(String str, int idx){
+        if(idx == 0){
+            System.out.println(str.charAt(idx));
+            return;
+        }
+
+        System.out.println(str.charAt(idx));
+        printRev(str, idx-1);
+    }
+  public static void main(String[] args) {
+    String str = "abcd";
+    printRev(str, str.length()-1);
+  }
+}
+
