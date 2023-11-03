3c46fc3fee437932a26ef853546675a16e6ff64b
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2023-11-03 21:44:05 +0530

    Add files via upload

---
+++ countTotalPath.java
@@ -0,0 +1,25 @@
+public class Recursion3 {
+   //Qs.Count total paths in a maze to move from(0,0) to (n,m)
+    public static int countPaths(int i, int j, int n, int m){
+        if(i == n || j == m){
+            return 0;
+        }
+        if(i == n-1 && j == m-1){
+            return 1;
+        }
+        //move downwards
+        int downPaths = countPaths(i+1, j, n, m);
+
+        //move right
+        int rightPaths = countPaths(i, j+1, n, m);
+        return downPaths+rightPaths;
+    }
+    public static void main(String[] args) {
+        int n = 3, m = 3;
+        int totalPaths = countPaths(0, 0, n, m);
+        System.out.println(totalPaths);
+
+    }
+
+
+}
