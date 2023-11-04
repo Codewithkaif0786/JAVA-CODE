f914815d0463e554b3f044046f1a8b14b2630f3d
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2023-11-04 10:50:49 +0530

    Add files via upload

---
+++ printAllPermutation.java
@@ -0,0 +1,19 @@
+public class Recursion3 {
+  public static void printPerm(String str, String permutation){
+        if(str.length() == 0){
+            System.out.println(permutation);
+            return;
+        }
+        for(int i=0; i<str.length(); i++){
+            char currChar = str.charAt(i);
+            //"abc" -> "ab"
+            String newstr = str.substring(0, i) + str.substring(i +1);
+            printPerm(newstr, permutation+currChar);
+        }
+    }
+    public static void main(String[] args) {
+        
+        String str = "abc";
+        printPerm(str, "");
+    }
+}
