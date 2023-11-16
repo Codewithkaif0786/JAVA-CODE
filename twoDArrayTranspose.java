9b3b504a3f21dd12b3ea81937cb19f60b982b350
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2023-11-16 21:45:32 +0530

    Add files via upload

--- twoDArrayTranspose.java
+++ twoDArrayTranspose.java
@@ -1,13 +1,27 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
-            }
-        }
-        return new int[]{}; // No solution found
-    }
+import java.util.Scanner;
+class Suduk{
+    //For a given matrix of N x M, print its transpose.
+     public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      int m = sc.nextInt();
+
+
+      int matrix[][] = new int[n][m];
+      for(int i=0; i<n; i++) {
+           for(int j=0; j<m; j++) {
+               matrix[i][j] = sc.nextInt();
+           }
+      }
+
+
+      System.out.println("The transpose is : ");
+      //To print transpose
+      for(int j=0; j<m ;j++) {
+          for(int i=0; i<n; i++) {
+              System.out.print(matrix[i][j]+" ");
+          }
+          System.out.println();
+      }
+   }
 }
