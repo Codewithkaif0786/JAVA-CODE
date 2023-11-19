7d06b649d45ae528dec9b266caceaff6183283b1
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2023-11-19 09:04:22 +0530

    Add files via upload

---
+++ append.java
@@ -0,0 +1,13 @@
+public class stringbuilder {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("K");
+
+        //.append  means add char one bye one
+
+        sb.append('a');
+        sb.append('i');
+        sb.append('f');
+
+        System.out.println(sb);
+    }
+}
