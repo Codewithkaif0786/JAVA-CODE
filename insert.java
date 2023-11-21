e54733ac1601774161c43a7fa8d27ade0f9c0b08
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2023-11-21 19:06:04 +0530

    Add files via upload

---
+++ insert.java
@@ -0,0 +1,10 @@
+public class server {
+
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder("Kif");
+        //insert a
+        sb.insert(1, 'a');
+        System.out.println(sb);
+    }
+}
