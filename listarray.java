c9363ec376e1c8922777452bf1ba000bbad6108b
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2023-12-03 21:12:43 +0530

    Add files via upload

---
+++ listarray.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+  public class ArrayList{
+    public static void main(String args[]){
+     ArrayList<Integer> list = new AraayList<integer>();
+      
+     //add elements
+     list.add(0);
+     list.add(1);
+     list.add(2);
+
+
+    System.out.println(list);
+   }
+}
